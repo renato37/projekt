0145246b764a1d33870af13e9adb03fbd31fda97
renato37 <67154098+renato37@users.noreply.github.com>
2022-12-27 12:13:46 +0100

    Add files via upload

---
+++ pages/1_Statistical_analysis.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import researchpy as rp
+from default.read_defaults import *
+import numpy as np
+
+y_basic = data_basic['target']
+
+tab1, tab2, tab3 = st.tabs(["T-test", "Boxplot", "Scatterplot"])
+selectedColumns = {}
+
+for c in data_basic.columns:
+   selectedColumns[c] = True
+
+with st.sidebar:
+   with st.expander("Select columns"):
+      for c in data_basic.columns:
+         selectedColumns[c] = st.checkbox(c, selectedColumns[c])
+
+filteredColumns = [k[0] for k in selectedColumns.items() if (k[1] is True)]
+
+with tab1:
+   col21, col22 = st.columns(2)
+   with col21:
+      st.write(texts['t-testAbstract'])
+   with col22:
+      selectSec = filteredColumns.copy()
+      selectSec.remove('target')
+      select1 = st.selectbox('', set(filteredColumns))
+
+      summary, results = rp.ttest(data_basic[select1][data_basic['target']==0], data_basic[select1][data_basic['target']==1])
+      st.write(summary)
+
+with tab2:
+   col21, col22 = st.columns(2)
+   with col21:
+      st.write(texts['boxplotAbstract'])
+   with col22:
+      i = 0
+      fig, axs = plt.subplots(math.ceil(len(filteredColumns) / 2), 2, figsize=(10, len(filteredColumns)*2))
+      for col in filteredColumns:
+         axs[math.floor(i/2), i % 2].boxplot(data_basic[col])
+         axs[math.floor(i / 2), i % 2].set_title(col)
+         i+=1
+      st.pyplot(fig)
+
+with tab3:
+   st.write(texts['scatterCorrAbstract'])
+   numOfScatter = int(len(filteredColumns)*(len(filteredColumns)-1)/2)
+   fig, axs = plt.subplots(math.ceil(numOfScatter/2), 2, figsize=(10, numOfScatter*2.5))
+   k = 0
+   for i in range(0, len(filteredColumns)):
+      for j in range(i+1, len(filteredColumns)):
+         corr = np.corrcoef(data_basic[filteredColumns[i]], data_basic[filteredColumns[j]])
+         axs[math.floor(k / 2), k % 2].set_title(corr[0,1])
+         axs[math.floor(k / 2), k % 2].scatter(data_basic[filteredColumns[i]], data_basic[filteredColumns[j]])
+         axs[math.floor(k / 2), k % 2].set_xlabel(filteredColumns[i])
+         axs[math.floor(k / 2), k % 2].set_ylabel(filteredColumns[j])
+         k += 1
+   st.pyplot(fig)
---
+++ pages/2_Machine_Learning.py
@@ -0,0 +1,138 @@
+import streamlit as st
+import pandas as pd
+from default.data_distribution import *
+from default.machine_learning_models import *
+import matplotlib.pyplot as plt
+from default.read_defaults import *
+
+with st.sidebar:
+    splitPercent = st.slider('Chose percentage of data for test', 0, 100, 20)/100
+
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['General', 'Decision tree', 'Naive Bayes', 'Random forest', 'Logistic regression', 'SVM'])
+selectedColumns = {}
+
+for c in data_basic.columns:
+   selectedColumns[c] = True
+
+with st.sidebar:
+   with st.expander("Select columns"):
+      for c in data_basic.columns:
+          if(c != 'target'):
+            selectedColumns[c] = st.checkbox(c, selectedColumns[c])
+
+filteredColumns = [k[0] for k in selectedColumns.items() if (k[1] is True and k[0] != 'target')]
+
+data_filtered = data_basic[filteredColumns]
+
+y_basic = data_basic['target']
+X_basic = data_filtered
+
+X_train, X_test, y_train, y_test = splitData(X_basic, y_basic, splitPercent)
+
+with tab1:
+    st.write(texts["machineLearningAbstract"])
+
+with tab2:
+    col21, col22 = st.columns(2)
+    with col21:
+        st.write(texts["decisionTreeAbstract"])
+    with col22:
+        DT_basic_matrix, DT_basic_acc = getPrediction('Decision tree', X_train, X_test, y_train, y_test)
+
+        st.dataframe(DT_basic_matrix)
+        st.write(DT_basic_acc)
+
+        labels = 'True negatives', 'False negative', 'False positive', 'True positive'
+        sizes = [DT_basic_matrix[0][0], DT_basic_matrix[0][1], DT_basic_matrix[1][0], DT_basic_matrix[1][1]]
+        explode = (0.1, 0, 0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes,  labels=labels, explode=explode, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        st.pyplot(fig1)
+
+with tab3:
+    col21, col22 = st.columns(2)
+    with col21:
+        st.write(texts["nativeBayesAbstract"])
+    with col22:
+        DT_basic_matrix, DT_basic_acc = getPrediction('Naive Bayes', X_train, X_test, y_train, y_test)
+
+        st.dataframe(DT_basic_matrix)
+        st.write(DT_basic_acc)
+
+        labels = 'True negatives', 'False negative', 'False positive', 'True positive'
+        sizes = [DT_basic_matrix[0][0], DT_basic_matrix[0][1], DT_basic_matrix[1][0], DT_basic_matrix[1][1]]
+        explode = (0.1, 0, 0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes,  labels=labels, explode=explode, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        st.pyplot(fig1)
+
+with tab4:
+    col21, col22 = st.columns(2)
+    with col21:
+        st.write(texts["randomForestAbstract"])
+    with col22:
+        DT_basic_matrix, DT_basic_acc = getPrediction('Random forest', X_train, X_test, y_train, y_test)
+
+        st.dataframe(DT_basic_matrix)
+        st.write(DT_basic_acc)
+
+        labels = 'True negatives', 'False negative', 'False positive', 'True positive'
+        sizes = [DT_basic_matrix[0][0], DT_basic_matrix[0][1], DT_basic_matrix[1][0], DT_basic_matrix[1][1]]
+        explode = (0.1, 0, 0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes,  labels=labels, explode=explode, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        st.pyplot(fig1)
+
+with tab5:
+    col21, col22 = st.columns(2)
+    with col21:
+        st.write(texts["logisticRegressionAbstract"])
+    with col22:
+        DT_basic_matrix, DT_basic_acc = getPrediction('Logistic regression', X_train, X_test, y_train, y_test)
+
+        st.dataframe(DT_basic_matrix)
+        st.write(DT_basic_acc)
+
+        labels = 'True negatives', 'False negative', 'False positive', 'True positive'
+        sizes = [DT_basic_matrix[0][0], DT_basic_matrix[0][1], DT_basic_matrix[1][0], DT_basic_matrix[1][1]]
+        explode = (0.1, 0, 0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes,  labels=labels, explode=explode, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        st.pyplot(fig1)
+
+with tab6:
+    col21, col22 = st.columns(2)
+    with col21:
+        st.write(texts["SVMAbstract"])
+    with col22:
+        DT_basic_matrix, DT_basic_acc = getPrediction('SVM', X_train, X_test, y_train, y_test)
+
+        st.dataframe(DT_basic_matrix)
+        st.write(DT_basic_acc)
+
+        labels = 'True negatives', 'False negative', 'False positive', 'True positive'
+        sizes = [DT_basic_matrix[0][0], DT_basic_matrix[0][1], DT_basic_matrix[1][0], DT_basic_matrix[1][1]]
+        explode = (0.1, 0, 0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes,  labels=labels, explode=explode, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        st.pyplot(fig1)
