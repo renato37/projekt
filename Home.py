6aaff28151edf2598f813cab3f3c15197184d55d
renato37 <67154098+renato37@users.noreply.github.com>
2023-01-03 21:38:46 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 from default.read_defaults import *
 
-st.write(texts['medicalAnalysisMainText'])
+tab1, tab2 = st.tabs(['Usual', 'About data'])
+with tab1:
+    st.write(texts['medicalAnalysisMainText'])
+with tab2:
+    st.write(texts['contextData'])
+    st.write(texts['aboutData'])
